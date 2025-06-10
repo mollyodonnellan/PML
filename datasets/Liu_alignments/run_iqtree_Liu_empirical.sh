5d415502cfd8feae02abb8ee22d26e651f8f308c
mollyodonnellan <molly.donnellan@uri.edu>
2025-06-10 11:41:26 -0400

    Update run_iqtree_Liu_empirical.sh

--- datasets/Liu_alignments/run_iqtree_Liu_empirical.sh
+++ datasets/Liu_alignments/run_iqtree_Liu_empirical.sh
@@ -18,10 +18,10 @@ module load uri/main
 module load iq-tree/2.3.1
 
 #Path to empirical dataset as phylip file. CHANGE to the path for the folder containing files ending in ".phy"
-filesEmpirical="../PML/datasets/Liu_alignments/seq/cds/*.phy"
+filesEmpirical="/PML/datasets/Liu_alignments/seq/seq/cds/*.phy"
 
 #Concatenate input fasta files and prepare partitions ahead of IQTree run. CHANGE to the path for the folder containing the file "AMAS.py"
-python3 ../AMAS/amas/AMAS.py concat -f phylip -d dna --out-format fasta --part-format raxml -i ${filesEmpirical} -c 20 -t concatenatedTrain.fasta -p partitionsTrain.txt
+python3 /AMAS/amas/AMAS.py concat -f phylip -d dna --out-format fasta --part-format raxml -i ${filesEmpirical} -c 20 -t concatenatedTrain.fasta -p partitionsTrain.txt
 
 #Run IQtree. Flags: -nt: use 20 CPU cores -spp: specifies partition file but allows partitions to have different evolutionary speeds -pre: specifies prefix for output files -m: determine best fit model immediately followed by tree reconstruction -bb: sets 1000 bootstrap replicates  -alrt: sets 1000 replicates to perform SH-like approximate likelihood test (SH-aLRT)
 iqtree2-mpi -nt 20 -s concatenatedTrain.fasta -spp partitionsTrain.txt -pre inferenceEmpirical -m MFP -bb 1000 -alrt 1000
