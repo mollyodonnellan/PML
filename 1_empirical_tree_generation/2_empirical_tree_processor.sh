5f196a69afb1b0bddcf0c1cbd082250d45125f3b
mollyodonnellan <molly.donnellan@uri.edu>
2025-06-10 12:02:01 -0400

    Update 2_empirical_tree_processor.sh

--- 1_empirical_tree_generation/2_empirical_tree_processor.sh
+++ 1_empirical_tree_generation/2_empirical_tree_processor.sh
@@ -3,6 +3,9 @@
 #SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
+#SBATCH -p uri-cpu
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
 
@@ -13,6 +16,7 @@ cd ..
 pwd
 date
 
-module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
+module load uri/main
+module load R-bundle-Bioconductor/3.15-foss-2021b-R-4.2.0
 
-Rscript empirical_tree_generation/empirical_tree_processor.R
+Rscript 1_empirical_tree_generation/empirical_tree_processor.R
