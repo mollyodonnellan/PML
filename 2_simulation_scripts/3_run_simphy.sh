40249ee96548c3e4cc42878e426631efe21509e8
mollyodonnellan <molly.donnellan@uri.edu>
2025-06-24 13:10:01 -0400

    Update 3_run_simphy.sh

--- 2_simulation_scripts/3_run_simphy.sh
+++ 2_simulation_scripts/3_run_simphy.sh
@@ -3,6 +3,9 @@
 #SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
+#SBATCH --mail-type=ALL
+#SBATCH -p uri-cpu
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
 
