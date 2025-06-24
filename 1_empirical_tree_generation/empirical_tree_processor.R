0f97c4a44ee31b45591cf2ec371b6bb903ddb35f
mollyodonnellan <molly.donnellan@uri.edu>
2025-06-24 08:54:41 -0400

    Update empirical_tree_processor.R

--- 1_empirical_tree_generation/empirical_tree_processor.R
+++ 1_empirical_tree_generation/empirical_tree_processor.R
@@ -51,21 +51,6 @@ ggsave (s, file="simulations/empirical/wickett/1/wickett_tree_um.png")
 write.tree(wickett_tree_um, "simulations/empirical/wickett/1/s_tree.trees", digits=8)
 write(c(20002,100000),"simulations/empirical/wickett/generate_params.txt")
 
-
-#Empirical species tree 3 (McGowen et al.):
-
-
-mcgowen <- read.tree("simulations/empirical/mcgowen/1/inferenceEmpirical.treefile")
-mcgowen_um <- chronos(mcgowen)
-class(mcgowen_um) <-"phylo"
-mcgowen_scale <- 75000000/20
-mcgowen_um <- rescale(mcgowen_um, model = "depth", mcgowen_scale)
-mcgowen_um$tip.label <- as.character(1:length(mcgowen_um$tip.label))
-t <- ggtree(mcgowen_um) + theme_tree2()
-ggsave(t, file="simulations/empirical/mcgowen/1/mcgowen_um.png")
-write.tree(mcgowen_um, "simulations/empirical/mcgowen/1/s_tree.trees", digits=8)
-write(c(20003,1000),"simulations/empirical/mcgowen/generate_params.txt")
-
 #Empirical species tree 4 (Liu et al.):
 
 liu_tree <- read.tree("simulations/empirical/liu/1/inferenceEmpirical.treefile")
