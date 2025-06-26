214e3d1fb43d7f79d93fa054e1225962f3977112
mollyodonnellan <molly.donnellan@uri.edu>
2025-06-26 10:53:57 -0400

    Update 6_run_iqtree_array.sh

--- 3_feature_assessment/6_run_iqtree_array.sh
+++ 3_feature_assessment/6_run_iqtree_array.sh
@@ -9,6 +9,10 @@
 
 date
 
+module load mpich/4.2.1
+module load uri/main
+module load iq-tree/2.3.1
+
 for i in ../simulations/*/*/1/alignmentGroups/
 
 do
@@ -20,9 +24,6 @@ do
 	#path to second half of gene alignments for dataset
 	aligned_loci_path3="../alignments3"
 
-	#path to iqtree executable; replace with your own path if not running on Andromeda
-	iqtree_exe="/home/aknyshov/alex_data/andromeda_tools/iqtree-2.1.2-Linux/bin/iqtree2"
-
  
 	#create a series of arrays corresponding to each line in the array_list.txt file
 	fileline=$(sed -n "${SLURM_ARRAY_TASK_ID}"p array_list.txt)
@@ -32,9 +33,9 @@ do
 	do
 		cd ../iqtree_genetrees2
 
-		$iqtree_exe --keep-ident -nt 2 -s ${aligned_loci_path2}/${line} -pre inference_${line} -m MFP -bb 1000 -alrt 1000 #iqtree job. Flags instruct iqtree to keep sequence identifiers as they are in the input file; to set 2 threads for parallel processing; specifies a DNA aligment file; specifies a prefix for the output files; specifies the substitution model to be used, MFP, a mixture model of amino acid frequencies; sets 1000 ultrafast bootstraps; and ets the number of replicates for the non-parametric approximate likelihood ratio test (aLRT) to 1000 
+		iqtree2-mpi --keep-ident -nt 2 -s ${aligned_loci_path2}/${line} -pre inference_${line} -m MFP -bb 1000 -alrt 1000 #iqtree job. Flags instruct iqtree to keep sequence identifiers as they are in the input file; to set 2 threads for parallel processing; specifies a DNA aligment file; specifies a prefix for the output files; specifies the substitution model to be used, MFP, a mixture model of amino acid frequencies; sets 1000 ultrafast bootstraps; and ets the number of replicates for the non-parametric approximate likelihood ratio test (aLRT) to 1000 
 		cd ../iqtree_genetrees3
-		$iqtree_exe --keep-ident -nt 2 -s ${aligned_loci_path3}/${line} -pre inference_${line} -m MFP -bb 1000 -alrt 1000
+		iqtree2-mpi --keep-ident -nt 2 -s ${aligned_loci_path3}/${line} -pre inference_${line} -m MFP -bb 1000 -alrt 1000
 		cd ../alignmentGroups
 	done
 
