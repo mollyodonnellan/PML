b1893d7d10554c3273097d86c24f1b837729a959
mollyodonnellan <molly.donnellan@uri.edu>
2025-07-14 11:24:16 -0400

    Update 5_INDELible.sh

--- 2_simulation_scripts/5_INDELible.sh
+++ 2_simulation_scripts/5_INDELible.sh
@@ -8,6 +8,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=6G
+#SBATCH --array=[0-48]%49
 
 ##This script prepares directories and control files for INDELible, runs INDELible for all four empirical datasets, and modifies INDELible alignments to introduce crosscontamination and missing data 
 
@@ -20,19 +21,21 @@ date
 module load uri/main
 module load R-bundle-Bioconductor/3.15-foss-2021b-R-4.2.0
 
-for i in ../simulations/*/*/1/
-	do
+SPP=(../simulations/*/*/1/)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
+INDEL=(../../../../../../INDELibleV1.03/src/indelible)
+
 	cd $i
 	pwd
-	Rscript ../../../../simulation_scripts/prep_INDELible.R gene_trees.tre df.csv #CHANGE to own path to the script
+	Rscript ../../../../2_simulation_scripts/prep_INDELible.R gene_trees.tre df.csv
 	cd alignments1
 	cp ../control.txt .
-	/data/schwartzlab/awalling/tools/INDELibleV1.03/src/indelible #CHANGE to own path to INDELible
+	$INDEL
 	cp ../controlCDS.txt ./control.txt
-	/data/schwartzlab/awalling/tools/INDELibleV1.03/src/indelible #CHANGE to own path to INDELible
+        $INDEL
 	cd ../
-	Rscript ../../../../simulation_scripts/post_INDELible.R alignments1 df.csv #CHANGE to own path to the script
-	cd ../../../../simulation_scripts/
+	Rscript ../../../../2_simulation_scripts/post_INDELible.R alignments1 df.csv
+	cd ../../../../2_simulation_scripts/
 	pwd
-done
+
 
