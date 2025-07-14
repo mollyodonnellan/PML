3335cc940e7ddea79269993db2d6fa59c047d5ec
mollyodonnellan <molly.donnellan@uri.edu>
2025-07-14 11:22:50 -0400

    Update 4_post_simphy.sh

--- 2_simulation_scripts/4_post_simphy.sh
+++ 2_simulation_scripts/4_post_simphy.sh
@@ -3,6 +3,7 @@
 #SBATCH --time=72:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks-per-node=1   # processor core(s) per node
+#SBATCH --array=[0-48]%49
 #SBATCH -p uri-cpu
 #SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
 #SBATCH --mail-type=ALL
@@ -14,22 +15,27 @@
 pwd
 date
 
+# create an array variable containing the folder names
+SPP=(../simulations/*/*/)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
 
-for i in ../simulations/*/*; do 
-    echo $i; 
-    while [[ $(ls ${i}/1/loc_*/1/g_trees1.trees | wc -l) -lt 2000 ]]; do
+echo "Processing: $i"
+
+if [[ $(wc -l < ${i}1/gene_trees.tre) -lt 2000 ]]; then 
+    while [[ $(ls ${i}1/loc_*/1/g_trees1.trees | wc -l) -lt 2000 ]]; do
         for j in $(seq 1 2000); do # go through all loc_ (2000)
-            if [ -s ${i}/1/loc_${j}/1/g_trees1.trees ]
+            if [ -s ${i}1/loc_${j}/1/g_trees1.trees ]
                 then echo "$i $j completed"
-            else 
-                echo "$i $j did not complete. Rerunning"
-                $(grep -w ${i}/1/loc_${j} 3_run_simphy_command_list.txt)
+            else
+                echo "$i loc_$j is not complete. Running"
+                $(grep -w ${i}1/loc_${j} 3_run_simphy_command_list.txt)
             fi
         done
     done
    
-    cat ${i}/1/loc_*/1/g_trees1.trees >> ${i}/1/gene_trees.tre
-    rm -rf ${i}/1/loc_*
-done
+    cat ${i}1/loc_*/1/g_trees1.trees > ${i}1/gene_trees.tre
+    rm -rf ${i}1/loc_*
+else echo "gene_trees.tre already exists - skipping."
+fi
 
 
