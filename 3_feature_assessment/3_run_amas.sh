e4120fc2dbff4498d7cceb1a3e65aca4c40580fc
mollyodonnellan <molly.donnellan@uri.edu>
2025-07-15 12:01:57 -0400

    Update 3_run_amas.sh

--- 3_feature_assessment/3_run_amas.sh
+++ 3_feature_assessment/3_run_amas.sh
@@ -6,7 +6,6 @@
 #SBATCH -p uri-cpu
 #SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
 #SBATCH --mail-type=ALL
-#SBATCH --array=[1-49]%8
 
 module purge
 module load Python/3.7.4-GCCcore-8.3.0
@@ -16,14 +15,14 @@ cores=12
 pwd
 date
 
-SPP=(../simulations/*/*/1/)
-i=${SPP[$SLURM_ARRAY_TASK_ID]}
-
+for i in ../simulations/*/*/1
+        do
 	cd ${i}
 	pwd
 	ls
 	python ${amas} summary -c ${cores} -o amas_output2.txt -f fasta -d dna -i alignments2/*.fas 
 	python ${amas} summary -c ${cores} -o amas_output3.txt -f fasta -d dna -i alignments3/*.fas
 	cd ../../../../3_feature_assessment/
+done
 
 date
