65079eda0c320e2763297e1425a9d7ae5a6059cb
mollyodonnellan <molly.donnellan@uri.edu>
2025-07-16 10:37:35 -0400

    Update 2_run_mafft.sh

--- 3_feature_assessment/2_run_mafft.sh
+++ 3_feature_assessment/2_run_mafft.sh
@@ -5,22 +5,23 @@
 #SBATCH -p uri-cpu
 #SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
 #SBATCH --mail-type=ALL
+#SBATCH --array=[0-48]%49
 
 ### adjust/add sbatch flags as needed
 
 module load uri/main
 module load MAFFT/7.505-GCC-11.3.0-with-extensions 
 
-for i in ../simulations/*/*/1/alignmentGroups
-do
+SPP=(../simulations/*/*/1/alignmentGroups)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
+
 	cd ${i}
 	pwd
-	fileline=$(sed -n ${SLURM_ARRAY_TASK_ID}p array_list.txt)
-	
+	fileline=$(sed -n '1,8'p array_list.txt)
+	echo "Processing $fileline"	
 	
 	cat ${fileline} | while read line
 	do
 		mafft --auto --thread 4 ../alignments2/${line} > ../alignments3/${line}
 	done
 	cd ../../../../../3_feature_assessment
-done
