9f18bb21c046404a28c60be226ebaa12a1905b25
mollyodonnellan <molly.donnellan@uri.edu>
2025-07-17 12:24:37 -0400

    Update 1_run_prep.sh

--- 3_feature_assessment/1_run_prep.sh
+++ 3_feature_assessment/1_run_prep.sh
@@ -8,13 +8,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH -c 1
 #SBATCH --mem-per-cpu=6G
-#SBATCH -p uri-cpu
-#SBATCH --mail-user="molly.donnellan@uri.edu" #CHANGE TO user email address
-#SBATCH --mail-type=ALL
+#SBATCH --array=[0-48]%49
 
 date
-for i in ../simulations/*/*/1
-	do
+
+SPP=(../simulations/*/*/1)
+i=${SPP[$SLURM_ARRAY_TASK_ID]}
+
 	cd ${i}
 	pwd
 	ls
@@ -33,5 +33,5 @@ for i in ../simulations/*/*/1
 	mkdir iqtree_concattree
 	mkdir astral_tree
 	cd ../../../../2_simulation_scripts
-done
+
 date
